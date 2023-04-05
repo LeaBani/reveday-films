8f5f5d6b2f9a2a32dab7484bcd4f5ba89a5d0732
LeaBani <113905453+LeaBani@users.noreply.github.com>
2023-04-05 19:18:08 +0200

    repalce brace

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,14 +1,14 @@
 import {
   Circle, Stack, useColorMode, useMediaQuery, Flex, Box, Text, Button, Image,
 } from '@chakra-ui/react';
-import './style.scss';
+
 import profile from '../../assets/profile.jpg';
 
 function Header() {
   const { colorMode } = useColorMode();
   const isDark = colorMode === 'dark';
 
-  const { isNotSmallerScreen } = useMediaQuery('(min-width:800px)');
+  const [isNotSmallerScreen] = useMediaQuery('(min-width: 800px)');
 
   return (
     <Stack>
@@ -29,7 +29,7 @@ function Header() {
         <Box mt={isNotSmallerScreen ? '0' : '16'} align="flex-start">
           <Text fontSize="2rem" fontWeight="semiBold">Bonjour, je suis</Text>
           <Text fontSize="5rem" fontWeight="bold" bgGradient="linear(to-r, cyan.400, blue.500, purple.600)" bgClip="text">Favian Ramos</Text>
-          <Text fontSize="1rem" color={isDark ? 'gray.200' : 'gray.500'}>Vidéaste cinématographique de votre mariage</Text>
+          <Text fontSize="1rem" color={isDark ? 'gray.200' : 'gray.500'}>Irure laborum tempor do excepteur do consequat sint id est sit veniam nisi fugiat. Amet amet non id quis. Nulla irure id proident sit laborum qui id laboris deserunt id ut et cillum id.</Text>
           <Button mt={8} colorScheme="blue" onClick={() => window.open('https://test.com')}>Contactez-moi</Button>
           <Image alignSelf="center" mt={isNotSmallerScreen ? '0' : '12'} mb={isNotSmallerScreen ? '0' : '12'} borderRadius="full" backgroundColor="transparent" boxShadow="lg" boxSize="300px" src={profile} />
         </Box>
--- src/components/Profile/index.jsx
+++ src/components/Profile/index.jsx
@@ -1,9 +1,10 @@
 import {
   Heading, Text, Box, Flex, useMediaQuery, Icon,
 } from '@chakra-ui/react';
+import { DiAndroid } from 'react-icons/di';
 
 function Profile() {
-  const { isNotSmallerScreen } = useMediaQuery('(min-width:600px)');
+  const [isNotSmallerScreen] = useMediaQuery('(min-width: 600px)');
 
   return (
     <Flex
@@ -23,9 +24,13 @@ function Profile() {
       </Box>
       <Box alignSelf="center" px="32" py="16">
         <Text fontWeight="bold" fontSize="24">Videaste de mariage</Text>
-        <Flex direction={isNotSmallerScreen ? 'row' : 'column'} mt={8}>
-          <Flex rounded="x1" direction="column" mt={4} bg="blue.400" h="30vh" w="30vh" justify="flex-end">
+        <Flex direction={isNotSmallerScreen ? 'row' : 'column'} mt="8">
+          <Flex rounded="20" direction="column" mt="4" bg="blue.400" h="30vh" w="30vh" justify="flex-end" _hover={{ bg: 'teal.400' }}>
+            <Icon color="white" p="4" as={DiAndroid} w="24" h="24" />
             <Text color="white" p="4" fontSize="x1" fontWeight="semibold">App</Text>
+          </Flex>
+          <Flex rounded="20" direction="column" mt="4" bg="blue.400" h="30vh" w="30vh" justify="flex-end" _hover={{ bg: 'teal.400' }}>
+            <Text color="white" p="4" fontSize="x1" fontWeight="semibold">App 2</Text>
           </Flex>
         </Flex>
       </Box>
